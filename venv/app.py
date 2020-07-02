a19980ce806e0ee99e214977220e12f8559ac807
Jc Vasquez <john.vasquez@mdscsi.com>
2020-07-02 21:39:10 +0800

    Initial commit for basic flask server

---
+++ venv/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'hello world'
+
+@app.route('/home')
+def home():
+    return 'Welcome Home!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
